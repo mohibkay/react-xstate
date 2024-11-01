ec40e45f349e39cc07fd7d3688dfea709099d05f
mohibul-khan <mohibul.khan@flipkart.com>
2024-11-02 00:41:44 +0530

    context & assign

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,11 @@
 
 import { useMachine } from '@xstate/react';
 import { todosMachine } from '@/machine/todoAppMachine';
-import { myMachine } from '@/machine/myFirstMachine';
 
 export default function Home() {
-	// const [state, send] = useMachine(todosMachine, {
-	//   services: {
-	//     loadTodos: async () => {
-	//       return ["Take bin out", "do laundary"];
-	//     },
-	//   },
-	// });
-
 	const [state, send] = useMachine(todosMachine, {
 		services: {
 			loadTodos: async () => {
-				throw new Error('Failed to load todos');
 				return ['Take bin out', 'do laundary'];
 			},
 		},
@@ -25,27 +15,7 @@ export default function Home() {
 	return (
 		<main>
 			<pre>{JSON.stringify(state.value)}</pre>
-			{/* <pre>{JSON.stringify(state.value)}</pre> */}
-			<button
-				onClick={() => {
-					send({
-						type: 'Todos loaded',
-						todos: ['Take bin out', 'do laundary'],
-					});
-				}}
-			>
-				Todos loaded
-			</button>
-			<button
-				onClick={() => {
-					send({
-						type: 'Loading todos failed',
-						errorMessage: 'Failed to load todos',
-					});
-				}}
-			>
-				Loading todos failed
-			</button>
+			<pre>{JSON.stringify(state.context)}</pre>
 		</main>
 	);
 }
--- machine/todoAppMachine.ts
+++ machine/todoAppMachine.ts
@@ -1,18 +1,19 @@
-import { createMachine } from 'xstate';
+import { createMachine, assign } from 'xstate';
 
 export const todosMachine = createMachine(
 	{
 		/** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogBMAZgDsFAKyPbATgCMXxwBZbLQA2L3sggBoQAE9EAA4vCntbW1j7Dy0XDyCte1CAXzzIoRwCEk4JelJGITYwACc61DqKIwAbFQAzJoBbCmKRMvFaSuqZOQUlc01dfUsTWDM1UksbBB97Ck97eJd4vxcHWz9ImIRkvwpY1O9snK0snILCkFJ0OEt+0rE50ymVxAAtBFooCghQtBCtLFkkFvC4XKEggUijISqJysMpDUfgs-khrIgAidEB4NgiPB5bJldl4-AFYsiQJ90YIxpgKpAcYsLPjVnTnF4grZHE4obYggFgacvFpbBRHLTHAraRTxY4PIzmYNqJiqphlGz6o06pz8fNucteYS-AKhSLHGKJeLiWsRfKndk-L5BfYXE88kA */
 		id: 'Todo Machine',
 		schema: {
-			// events: {} as
-			// | { type: 'Todos loaded'; todos: string[] }
-			// | { type: 'Loading todos failed'; errorMessage: string },
 			services: {} as {
 				loadTodos: {
 					data: string[];
 				};
 			},
+		},
+		context: {
+			todos: [] as string[],
+			errorMessage: undefined as undefined | string,
 		},
 		tsTypes: {} as import('./todoAppMachine.typegen').Typegen0,
 		states: {
@@ -22,11 +23,13 @@ export const todosMachine = createMachine(
 					onDone: [
 						{
 							target: 'Todos Loaded',
+							actions: 'assignTodosToContext',
 						},
 					],
 					onError: [
 						{
 							target: 'Loading todos errored',
+							actions: 'assignErrorToContext',
 						},
 					],
 				},
@@ -37,5 +40,18 @@ export const todosMachine = createMachine(
 
 		initial: 'Loading Todos',
 	},
-	{},
+	{
+		actions: {
+			assignTodosToContext: assign((context, event) => {
+				return {
+					todos: event.data,
+				};
+			}),
+			assignErrorToContext: assign((context, event) => {
+				return {
+					errorMessage: (event.data as Error).message,
+				};
+			}),
+		},
+	},
 );
